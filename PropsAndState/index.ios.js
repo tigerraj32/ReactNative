9f8349e42d5a1736102ec01ecc9a29d014cf57fd
rajan <rajantwnabashu@gmail.com>
2016-05-19 16:44:16 +0545

    ios support

--- PropsAndState/index.ios.js
+++ PropsAndState/index.ios.js
@@ -1,53 +1,78 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
+ /**
+  * Sample React Native App
+  * https://github.com/facebook/react-native
+  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, { Component,PropTypes } from 'react';
+ import {
+   AppRegistry,
+   StyleSheet,
+   Text,
+   View
+ } from 'react-native';
 
-class PropsAndState extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ class SampleComponent extends Component {
+   render(){
+     return(
+       <Text onPress = {this.props.incrementCount}> Press here to increase count  </Text>
+     );
+   }
+ }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+ SampleComponent.propsTypes = {
+   incrementCount: React.PropTypes.func.isRequired,
+ }
+
+ class PropsAndState extends Component {
+
+   constructor(props) {
+     super(props);
+     this.incrementCount = this.incrementCount.bind(this);
+     this.state = {
+       val: 0,
+     }
+   }
+
+   incrementCount(){
+     console.log('<><><><><><><><><><><><><><>');
+     result = this.state.val + 1;
+     this.setState({val: result});
+   }
+
+   render() {
+     return (
+       <View style={styles.container}>
+         <Text style={styles.welcome}>
+           {this.state.val}
+         </Text>
+         <SampleComponent style={styles.instructions}  incrementCount= {this.incrementCount}/>
+       </View>
+     );
+   }
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   welcome: {
+     fontSize: 20,
+     textAlign: 'center',
+     margin: 10,
+   },
+   instructions: {
+     textAlign: 'center',
+     color: '#333333',
+     marginBottom: 5,
+   },
+ });
+
 
 AppRegistry.registerComponent('PropsAndState', () => PropsAndState);
